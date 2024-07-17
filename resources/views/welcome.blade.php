9ad75392cfdd9afeab74ebf110b66853e98632be
javad <sjht@gmail.com>
2024-07-17 14:23:56 +0330

    d

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<form method="post" action="{{ route('location') }}" enctype="multipart/form-data">
+<form method="post" action="{{ route('location2') }}" enctype="multipart/form-data">
     <div class="mb-3">
       <label class="form-label">latitude</label>
       <input name="lat" class="form-control" >
