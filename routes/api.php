1ce02dce3e512df90b57ccd44e7606e8ce0d2785
javad <sjht@gmail.com>
2024-07-17 16:31:51 +0330

    d

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SomethingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'v1',
+    'middleware' => 'with_fast_api_key'
+], function () {
+
+    Route::post('/just/an/example', [SomethingController::class, 'justAnExample']);
+
+    // ...
 });
