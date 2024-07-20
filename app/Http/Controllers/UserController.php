fe014ff93637cde112cf95e25212de307dda33ef
javad <sjht@gmail.com>
2024-07-20 15:33:33 +0330

    s

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Adrianorosa\GeoLocation\GeoLocation;
 use Geocoder\Location;
 use Illuminate\Http\Request;
+use League\CommonMark\Node\Block\Document;
 use Torann\GeoIP\Facades\GeoIP;
 
 
@@ -17,6 +18,19 @@ class UserController extends Controller
           $long = $request->long;
 
          return view('user' , compact('lat','long'));
+    }
+
+    public function showMap(lat $lat , long $long)
+    {
+
+          var  coord = { lat:lat , long:long};
+        new  google.maps.Map(document.getElementById("map"),
+         {
+            zoom:10 ,
+            center:coord
+         }
+    );
+
     }
 }
 
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,6 +4,10 @@
         <div class="container">
             <h1>CURRENT LOCATION </h1>
             <hr>
+            <div class="container" >
+            <div id="map" style="width:100% ; height:300ex"></div>
+            </div>
+
         </div>
     </body>
 </html>
