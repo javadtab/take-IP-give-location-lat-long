ec5cc89cb077172272f10a591a929aefa11ad15d
javad <sjht@gmail.com>
2024-07-20 15:18:54 +0330

    s

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,25 +4,6 @@
         <div class="container">
             <h1>CURRENT LOCATION </h1>
             <hr>
-
-
-           <!-- <div class="card">
-                <div class="cared-body">
-
-
-                    <h4> IP : {{$location->ip}}</h4>
-                    <h4> Country name : {{$location->countryName}}</h4>
-                    <h4> Country Code : {{$location->countryCode}}</h4>
-                    <h4> Region Code : {{$location->regionCode}}</h4>
-                    <h4> Region Name : {{$location->regionName}}</h4>
-                    <h4> City Name : {{$location->cityName}}</h4>
-                    <h4> Zip Code : {{$location->zipCode}}</h4>
-                    <h4> Latitude : {{$location->latitude}}</h4>
-                    <h4> Longitude : {{$location->longitude}}</h4>
-                    <h4> Timezone : {{$location->timezone}}</h4>
-
-                </div>
-            </div><!-->
         </div>
     </body>
 </html>
